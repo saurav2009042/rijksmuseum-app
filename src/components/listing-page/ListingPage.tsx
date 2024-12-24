f4e5edd985598ccd0e2a19aa9298d0731fa12945
sauravbose <saurav2009042@gmail.com>
2024-12-25 00:41:05 +0100

    Adding code description and maintaining consistent CSS nesting

--- src/components/listing-page/ListingPage.tsx
+++ src/components/listing-page/ListingPage.tsx
@@ -17,6 +17,11 @@ const ListingPage: React.FC = () => {
             setLoading(true);
 
             const artworksList: Array<IArtworkResponse> = await fetchArtworksList(query, currentPage);
+
+            /**
+             * The below approach ensures that when we display the artworks, each one has its own image URL.
+             * It therefore simplifies the rendering logic of our component.
+             */
             const artworksWithImages: Array<IArtworkResponseWithImages> = artworksList.map((artwork: IArtworkResponse) => ({
                 ...artwork,
                 imageUrl: artwork.webImage?.url,
@@ -32,20 +37,39 @@ const ListingPage: React.FC = () => {
         }
     }, []);
 
+    /**
+     * This is used to handle the initial loading of our application.
+     * With a blank search query and pageNumber = 1, the API will return 20 images.
+     */
     useEffect(() => {
         getArtworks('', 1);
     }, [getArtworks]);
 
+    /**
+     * This is used to handle the case when there is a search query or to reset to the initial state.
+     * The artworks are fetched corresponding to the search query with page number set to 1.
+     * If empty, the base state of the application is loaded.
+     */
     useEffect(() => {
         getArtworks(searchQuery, 1);
     }, [searchQuery, getArtworks]);
 
+    /**
+     * This is used to handle pagination.
+     * When user clicks on 'Load More' button, the page number gets incremented.
+     * As a result, the next 20 artworks are fetched and appended to the list.
+     */
     useEffect(() => {
         if (pageNumber > 1) {
             getArtworks(searchQuery, pageNumber);
         }
     }, [pageNumber, searchQuery, getArtworks]);
 
+    /**
+     * The main goal of this function is to set the inputValue as the searchQuery.
+     * We have inputValue to store whatever the user types, but it is not used to make a request to the server.
+     * Only when the user clicks on Submit button is when this function is called and the searchQuery is updated accordingly.
+     */
     const handleSearch = (e: React.FormEvent) => {
         e.preventDefault();
         setSearchQuery(inputValue);
